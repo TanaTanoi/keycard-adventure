f1961d28c7e558c45774e7380d771c48f8859524
hc09141 <hannahcraighead23@gmail.com>
2015-09-22 19:51:41 +1200

    Basic framework for the Item interface and tool abstract class

---
+++ CityGenJava/src/gameObjects/Item.java
@@ -0,0 +1,28 @@
+package gameObjects;
+
+/**
+ * The item interface describes objects in the game world that the player can interact with
+ * @author Hannah
+ *
+ */
+
+public interface Item {
+	
+	/**
+	 * Performs action on/with item
+	 * NOTE: may need a mouse click or object interacting with as param
+	 */
+	public void interact();
+	
+	/**
+	 * Returns the name of the item
+	 * @return item name
+	 */
+	public String getName();
+	
+	/**
+	 * Returns a description about the item
+	 * @return
+	 */
+	public String getDescription();
+}
---
+++ CityGenJava/src/gameObjects/Tool.java
@@ -0,0 +1,42 @@
+package gameObjects;
+
+/**
+ * The tool class describes a type of item that can be "equipped".
+ * When an item is equipped, it can be used to interact with the game  world
+ * @author Hannah
+ *
+ */
+
+public abstract class Tool implements Item {
+	
+	private String name;
+	private String description;
+	
+	public Tool(String name, String desc){
+		this.name = name;
+		description = desc;
+	}
+	
+	/**
+	 * Equip makes the tool the current selected tool
+	 */
+	public abstract void equip();
+	
+	/**
+	 * canUse determines whether this tool can be used in the
+	 * way the player is trying to use it
+	 * 
+	 * @return a boolean indicating whether the player can use
+	 * this tool
+	 * 
+	 * NOTE: may need click or interact item as param
+	 */
+	public abstract boolean canUse();
+	
+	/**
+	 * use defines the action that should be performed
+	 * when the item is used
+	 */
+	public abstract void use();
+	
+}
