9e17f86a9712ef75a6d2b72cd488479f7aab31ad
hc09141 <hc09141@users.noreply.github.com>
2015-09-30 10:06:28 +1300

    The player class now has an ID to identify it

--- CityGenJava/src/gameObjects/player/Player.java
+++ CityGenJava/src/gameObjects/player/Player.java
@@ -5,34 +5,37 @@ import gameObjects.world.Location;
 
 /**
  * The player class describes a character in the game
- * that is controlled by a user 
- * 
+ * that is controlled by a user
+ *
  * @author Hannah
  *
  */
 
 public class Player implements Character{
-	
+
 	private String name; // name selected by user
-	private Item [] inventory; // items  that the player carries 
-	private int health;
-	private Location loc; // describes player's location in game world	
-	
-	public Player(String name){
+	private Item [] inventory; // items  that the player carries
+	private int health = 100;
+	private Location loc; // describes player's location in game world
+	private int ID;
+
+	public Player(String name, int ID){
 		this.name = name;
+		this.ID = ID;
+		loc = new Location(0,0,0);
 	}
-	
+
 	/**
 	 * Moves the player's position in the game world
 	 * @param x is the x direction to move the player
 	 * @param z is the z direction to move the player
 	 */
-	
+
 	public void move(int x, int z){
 		loc.setX(x);
 		loc.setY(z);
 	}
-	
+
 	@Override
 	public Item talk(Character c){
 		return null;
@@ -40,9 +43,9 @@ public class Player implements Character{
 
 	@Override
 	public String getName() {
-		return name;		
+		return name;
 	}
-	
+
 	/**
 	 * Taking a potion has an effect on the character's health
 	 * This can be either a positive or a negative effect
@@ -51,7 +54,7 @@ public class Player implements Character{
 	public void takePotion(int effect){
 		health += effect;
 	}
-	
+
 	@Override
 	public void attack(int damage){
 		health -= damage;
@@ -66,5 +69,9 @@ public class Player implements Character{
 	public Location getLocation() {
 		return loc;
 	}
-	
+
+	public int getID(){
+		return ID;
+	}
+
 }
