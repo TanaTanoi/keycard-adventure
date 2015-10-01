2b09f2b84df85777e9ea19e645d4183198c4cc58
hc09141 <hc09141@users.noreply.github.com>
2015-10-01 16:39:59 +1300

    Furniture class is now not abstract as it should have no behaviour and does not be extended

--- CityGenJava/src/gameObjects/objects/Furniture.java
+++ CityGenJava/src/gameObjects/objects/Furniture.java
@@ -1,20 +1,46 @@
 package gameObjects.objects;
 
-public abstract class Furniture implements Item {
-	
+import gameObjects.player.Character;
+import gameObjects.world.Location;
+import javafx.geometry.BoundingBox;
+
+public class Furniture implements Item {
+
 	private String name;
 	private String description;
-	//private FurnitureStrategy strategy;
-	
-	
+	private Location loc;
+
 	@Override
 	public String getName(){
 		return name;
 	}
-	
+
 	@Override
 	public String getDescription(){
 		return description;
+	}
+
+	@Override
+	public void interact(Character c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void interact(Item i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public BoundingBox getBoundingBox() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Location getLocation() {
+		return loc;
 	}
 
 }
