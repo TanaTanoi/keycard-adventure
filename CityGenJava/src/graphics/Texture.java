d0093e33d836a9b0a92729df6b555c7be2786ab7
CDeighton <cullumdeighton@gmail.com>
2015-10-12 10:12:42 +1300

    Changed textures from clamping to repeating

--- CityGenJava/src/graphics/Texture.java
+++ CityGenJava/src/graphics/Texture.java
@@ -47,9 +47,9 @@ public class Texture {
 	        glBindTexture(GL_TEXTURE_2D, textureID); //Bind texture ID
 
 	        //Setup wrap mode
-	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
-	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
-	        
+	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
 	        //Setup texture scaling filtering
 	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
